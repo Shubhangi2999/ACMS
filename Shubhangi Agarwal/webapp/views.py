8419bf78e21a3c10097f865c298f903dd8783a7d
Shubhangi2999 <30768563+Shubhangi2999@users.noreply.github.com>
2020-05-07 14:24:44 +0530

    Update views.py

--- Shubhangi Agarwal/webapp/views.py
+++ Shubhangi Agarwal/webapp/views.py
@@ -21,14 +21,13 @@ def partners_list(request):
                 writer.writerow([partner.id,partner.address,partner.city,partner.state,partner.typestore,partner.size,partner.workingemployees,partner.customers,partner.service])
             return Response(status=status.HTTP_201_CREATED)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'GET':
         data = Partner.objects.all()
 
         serializer = PartnerSerializer(data, context={'request': request}, many=True)
 
         return Response(serializer.data)
-
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['PUT', 'DELETE'])
 def partners_detail(request, pk):
@@ -42,8 +41,9 @@ def partners_detail(request, pk):
         if serializer.is_valid():
             serializer.save()
             return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
     elif request.method == 'DELETE':
         partner.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
