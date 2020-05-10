33305a2de1e3191b4ead0b5e720360a8bc927992
Ayushi Gupta <ayushigupta8@gmail.com>
2020-05-10 22:17:44 +0530

    added value fields to store details

--- Ayushi_Gupta/login/src/Components/StoreDetails/index.js
+++ Ayushi_Gupta/login/src/Components/StoreDetails/index.js
@@ -26,27 +26,48 @@ export default class StoreDetails extends React.Component {
                     </FormGroup>
 
                     <FormGroup className='store-form-group'>
+                        <Label className='store-label' for="city">City</Label>
+                        <Input className='store-input' type="text" name="City" id="city" placeholder="Add your city" />
+                    </FormGroup>
+
+                    <FormGroup className='store-form-group'>
+                        <Label className='store-label' for="state">State</Label>
+                        <Input className='store-input' type="text" name="State" id="state" placeholder="Add your state" />
+                    </FormGroup>
+
+                    <FormGroup className='store-form-group'>
+                        <Label className='store-label' for="pincode">Pincode</Label>
+                        <Input className='store-input' type="text" name="pincode" id="pincode" placeholder="Add pincode" />
+                    </FormGroup>
+
+                    <FormGroup className='store-form-group'>
                         <Label className='store-label' for="store-type">Store Type</Label>
                         <Input className='store-input' type="text" name="Store Type" id="store-type" placeholder="Add store type" />
                     </FormGroup>
 
+
                     <FormGroup className='store-form-group'>
-                        <Label className='store-label' for="store-type">No of employees</Label>
+                        <Label className='store-label' for="store-size">Store Size</Label>
+                        <Input className='store-input' type="text" name="size" id="store-size" placeholder="Add store size" />
+                    </FormGroup>
+
+                    <FormGroup className='store-form-group'>
+                        <Label className='store-label' for="no-of-employees">No of employees</Label>
                         <Input className='store-input' type="text" name="No of employees" id="store-employees" placeholder="Add no. of employees" />
                     </FormGroup>
 
                     <FormGroup className='store-form-group'>
-                        <Label className='store-label' for="store-type">Service</Label>
+                        <Label className='store-label' for="no-of-customers">No. of customers</Label>
+                        <Input className='store-input' type="text" name="No of customers" id="no-of-customers" placeholder="Add no. of customers" />
+                    </FormGroup>
+
+                    <FormGroup className='store-form-group'>
+                        <Label className='store-label' for="service">Service</Label>
                         <Input className='store-input' type="text" name="Service" id="store-service" placeholder="Add service" />
                     </FormGroup>
 
                     <FormGroup className='store-form-group'>
-                        <Label className='store-label' for="store-type">Program</Label>
-                        <Input className='store-input' type="text" name="Program" id="store-program" placeholder="Add program" />
-                    </FormGroup>
-
-                    <FormGroup className='store-form-group'>
-                        <Label className='store-label' for="store-type">Program</Label>
+                        <Label className='store-label' for="program">Program</Label>
                         <Input className='store-input' type="text" name="Program" id="store-program" placeholder="Add program" />
                     </FormGroup>
                 </Form>
