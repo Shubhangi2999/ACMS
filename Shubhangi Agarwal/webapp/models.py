6cb67b48be836f5c1fadd2140e397319a29a093f
Shubhangi2999 <30768563+Shubhangi2999@users.noreply.github.com>
2020-05-12 23:11:50 +0530

    Add files via upload

--- Shubhangi Agarwal/webapp/models.py
+++ Shubhangi Agarwal/webapp/models.py
@@ -1,24 +1,30 @@
 from django.db import models
 
-class Partner(models.Model):
+
+class Customer(models.Model):
     id = models.AutoField(primary_key=True)
     firstName = models.CharField(max_length=32)
     lastName = models.CharField(max_length=32)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     contact = models.CharField(max_length=10)
     password = models.CharField(max_length=32)
-    confirmPassword = models.CharField(max_length=32)
-    storename = models.CharField(max_length=32)
-    address = models.CharField(max_length=300)
-    city = models.CharField(max_length=32)
-    pincode = models.CharField(max_length=6)
-    state = models.CharField(max_length=32)
-    typestore = models.CharField(max_length=32)
-    size = models.CharField(max_length=32)
-    workingemployees = models.PositiveIntegerField()
-    customer = models.PositiveIntegerField()
-    service = models.CharField(max_length=32)
-
 
     def __str__(self):
         return self.firstName
+
+
+class Store(models.Model):
+    ownerId = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    storeName = models.CharField(max_length=32)
+    address = models.CharField(max_length=512)
+    city = models.CharField(max_length=32)
+    state = models.CharField(max_length=32)
+    pincode = models.CharField(max_length=6)
+    typeStore = models.CharField(max_length=32)
+    size = models.CharField(max_length=32)
+    employees = models.PositiveIntegerField()
+    customers = models.PositiveIntegerField()
+    service = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.storeName
