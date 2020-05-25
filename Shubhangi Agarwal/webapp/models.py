2f3fcf261a5f41be94548ac216a6b8d51120d354
Shubhangi2999 <30768563+Shubhangi2999@users.noreply.github.com>
2020-05-25 21:13:06 +0530

    Updated changes

--- Shubhangi Agarwal/webapp/models.py
+++ Shubhangi Agarwal/webapp/models.py
@@ -2,12 +2,10 @@ from django.db import models
 
 
 class Customer(models.Model):
-#    user = models.OneToOneField(User, on_delete=models.CASCADE)
     email = models.CharField(max_length=64, primary_key=True)
     first_name = models.CharField(max_length=32, blank=True)
     last_name = models.CharField(max_length=32, blank=True)
     password = models.CharField(max_length=32)
-#    auth_token =
     contact = models.CharField(max_length=10)
 
 
@@ -17,16 +15,16 @@ class Customer(models.Model):
 
 class Store(models.Model):
     owner = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    storeName = models.CharField(max_length=32)
+    storename = models.CharField(max_length=32)
     address = models.CharField(max_length=512)
     city = models.CharField(max_length=32)
     state = models.CharField(max_length=32)
     pincode = models.CharField(max_length=6)
-    typeStore = models.CharField(max_length=32)
+    typestore = models.CharField(max_length=32)
     size = models.CharField(max_length=32)
-    employees = models.PositiveIntegerField()
-    customers = models.PositiveIntegerField()
+    workingemployees = models.PositiveIntegerField()
+    customer = models.PositiveIntegerField()
     service = models.CharField(max_length=32)
 
     def __str__(self):
-        return self.owner
+        return self.storename
--- Shubhangi Agarwal/webapp/views.py
+++ Shubhangi Agarwal/webapp/views.py
@@ -1,29 +1,39 @@
-#from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-#from django.contrib.auth import authenticate, login
 from rest_framework.response import Response
-from rest_framework.decorators import api_view #, renderer_classes
-#from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+from rest_framework.decorators import api_view
+from rest_framework.authentication import get_authorization_header, BaseAuthentication
 from rest_framework import status
 import jwt, json, csv
 from .models import Customer, Store
 from .serializers import *
 
 
-@api_view(['POST', 'GET'])
+@api_view(['POST'])
 def customer_signup(request):
     if request.method == 'POST':
-        serializer = CustomerSerializer(data=request.data)
+        form_data = {}
+        form_data = request.data
+        serializer = CustomerSerializer(data=form_data)
+
         if serializer.is_valid():
             serializer.save()
+            owner=Customer.objects.get(pk=form_data["email"])
+            store = Store(owner=owner,
+                          storename=form_data["storename"],
+                          address=form_data["address"],
+                          city = form_data["city"],
+                          state = form_data["state"],
+                          pincode = form_data["pincode"],
+                          typestore = form_data["typestore"],
+                          size = form_data["size"],
+                          workingemployees = form_data["workingemployees"],
+                          customer = form_data["customer"],
+                          service = form_data["service"])
+            store.save()
             return Response(status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'GET':
-        data = Customer.objects.all()
-        serializer = CustomerSerializer(data, context={'request': request}, many=True)
-        return Response(serializer.data)
     return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 def authenticate(email, password):
     try:
@@ -34,7 +44,7 @@ def authenticate(email, password):
         return 1
 
 
-@api_view(['POST', 'GET'])
+@api_view(['POST'])
 def login(request):
     if request.method == 'POST':
         data = {}
@@ -49,7 +59,19 @@ def login(request):
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST', 'GET'])
+@api_view(['GET'])
+def customer_details(request):
+    if request.method == 'GET':
+        auth = get_authorization_header(request).split()
+        token = auth[0]
+        payload = jwt.decode(token, "SECRET_KEY")
+        data = Customer.objects.filter(pk=payload["email"])
+        serializer = CustomerSerializer(data, context={'request': request}, many=True)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
 def add_store(request):
     if request.method == 'POST':
         serializer = StoreSerializer(data=request.data)
@@ -60,28 +82,27 @@ def add_store(request):
                 writer.writerow([serializer.data])
             return Response(status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'GET':
-        data = Store.objects.all()
+
+@api_view(['GET'])
+def store_details(request):
+    if request.method == 'GET':
+        auth = get_authorization_header(request).split()
+        token = auth[0]
+        payload = jwt.decode(token, "SECRET_KEY")
+        data = Store.objects.filter(owner=payload["email"])
         serializer = StoreSerializer(data, context={'request': request}, many=True)
         return Response(serializer.data)
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET','POST','PUT', 'DELETE'])
+@api_view(['PUT', 'DELETE'])
 def delete_store(request, id):
     try:
         store = Store.objects.get(pk=id)
     except Store.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-
-#    if request.method == 'PUT':
-#        serializer = StoreSerializer(store, data=request.data, context={'request': request})
-#        if serializer.is_valid():
-#            serializer.save()
-#            return Response(status=status.HTTP_204_NO_CONTENT)
-#        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
     if request.method == 'DELETE':
         store.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
